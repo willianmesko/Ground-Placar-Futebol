d6c1fe21ed3d8f10a0ba954be1b4aab37ba2beb5
Mesko <willianmesko@hotmail.com>
2020-01-15 15:38:47 -0300

    Update index.js

--- Frontend/src/components/PlacarBox/index.js
+++ Frontend/src/components/PlacarBox/index.js
@@ -3,7 +3,7 @@ import Card from "@material-ui/core/Card";
 import { Link } from "react-router-dom";
 import CardContent from "@material-ui/core/CardContent";
 import Container from "@material-ui/core/Container";
-import { PlacarInfo, useStyles } from "./styles";
+import { TimeInfo, useStyles, PlacarInfo } from "./styles";
 const moment = require("moment");
 
 export default function PlacarBox({ time }) {
@@ -27,33 +27,34 @@ export default function PlacarBox({ time }) {
 
   return (
     <Container maxWidth="sm" className={classes.pos}>
+
       <Card className={classes.card}>
         <p className={classes.champ}>{campeonatoNome}</p>
         <h5 className={classes.fase}>({fase})</h5>
         <CardContent className={classes.cardContent}>
-          <PlacarInfo>
+          <TimeInfo>
             <Link to={(idTimeCasa === 1083  && idTimeRival !== 1083) 
               ? `/` : `/timeinfo/${idTimeRival}`}>
               <img src={(idTimeCasa === 1083) ? logoTimeCasa : logoTimeRival} alt={(idTimeCasa === 1083) ? nomeTimeCasa : nomeTimeRival} />
             </Link>
             <span>{(idTimeCasa === 1083) ? siglaTimeCasa : siglaTimeRival}</span>
-          </PlacarInfo>
+          </TimeInfo>
 
-          <div>
+          <PlacarInfo>
             <p
               className={classes.placar}
             >{`${(idTimeCasa === 1083) ? golsTimeCasa : golsTimeRival} x ${(idTimeCasa === 1083) ? golsTimeRival : golsTimeCasa}`}</p>
             <h6 className={classes.fase}>
               {estadioNome} - {moment(data, "YYYY-MM-DD").format("DD/MM")}
             </h6>
-          </div>
+          </PlacarInfo>
 
-          <PlacarInfo>
+          <TimeInfo>
             <Link to={`/timeinfo/${(idTimeCasa === 1083) ? idTimeRival : idTimeCasa}`}>
               <img src={(idTimeCasa === 1083) ? logoTimeRival : logoTimeCasa} alt={nomeTimeRival} />
             </Link>
             <span>{(idTimeCasa === 1083) ? siglaTimeRival : siglaTimeCasa}</span>
-          </PlacarInfo>
+          </TimeInfo>
         </CardContent>
       </Card>
     </Container>
