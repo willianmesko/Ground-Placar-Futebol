b2767bd51fea4bdcfc07b6d952b61a5042855ad4
Mesko <willianmesko@hotmail.com>
2020-01-15 15:40:57 -0300

    Update styles.js

--- Frontend/src/pages/PlacarCampeonato/styles.js
+++ Frontend/src/pages/PlacarCampeonato/styles.js
@@ -6,7 +6,21 @@ export const CampeonatoInfo = styled.div`
   align-items: center;
   padding: 5px;
 
+  h1 {
+    @media (min-width: 320px) and (max-width: 414px) {
+      font-size: 15px;
+      }
+ 
+  }
+
   img {
     padding-top: 5px;
+    width: 120px;
+    height: 120px; 
+    @media (min-width: 320px) and (max-width: 414px){
+      width: 80px;
+      height: 80px; 
+    }
+ 
   }
 `;
