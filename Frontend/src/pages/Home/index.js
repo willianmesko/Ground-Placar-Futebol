2cc855dad64dcb58bf22f9896eb28158a821af12
Mesko <willianmesko@hotmail.com>
2020-01-15 09:39:53 -0300

    Update index.js

--- Frontend/src/pages/Home/index.js
+++ Frontend/src/pages/Home/index.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Campeonatos from "../../components/Campeonatos";
 import PlacarBox from "../../components/PlacarBox";
+import Button from '@material-ui/core/Button';
 import api from "../../services/api";
 import LoadingBoxPlacar from "../../components/loadingBox/placarBox";
 export default function Home() {
   const [placar, setPlacar] = useState([]);
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
+  const [visible, setVisible] = useState(3);
+   useEffect(() => {
     async function loadScore() {
       const result = await api.get("placartime/1083");
       setPlacar(result.data);
@@ -21,10 +22,19 @@ export default function Home() {
       <Campeonatos />
 
       {!loading ? (
-        placar.map(placar => <PlacarBox time={placar} />)
+        placar.slice(0, visible).map(placar => <PlacarBox time={placar} />)
       ) : (
         <LoadingBoxPlacar />
       )}
+      
+      {visible < placar.length && 
+        <div style={{display: "flex", justifyContent:"center"}}>
+          <Button onClick={() => setVisible(visible + 3)} variant="contained" color="primary" >
+             Carregar mais...
+          </Button>
+        </div>
+      }
+      
     </>
   );
 }
