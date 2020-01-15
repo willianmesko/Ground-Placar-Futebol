aa5f5b961c6d989ac0a43b6cdeeeb852560f3850
Mesko <willianmesko@hotmail.com>
2020-01-15 15:36:18 -0300

    Update styles.js

--- Frontend/src/components/Header/styles.js
+++ Frontend/src/components/Header/styles.js
@@ -1,11 +1,17 @@
 import { makeStyles } from "@material-ui/core/styles";
 import styled from "styled-components";
-
 export const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    marginBottom: theme.spacing(1)
-  }
+    marginBottom: theme.spacing(1),
+    height: "85px" ,
+    '@media (max-width: 320px)': {
+      height: "60px" ,
+    },
+    '@media ': {
+      height: "75px" ,
+    }
+  },
 }));
 
 export const Logo = styled.div`
@@ -15,5 +21,11 @@ export const Logo = styled.div`
   border-left: 1px solod #eee;
   img {
     height: 70px;
-  }
+    @media (max-width: 320px) {
+      height: 50px;
+    },
+    @media (max-width: 375px) {
+      height: 40px;
+    }
+    }
 `;
