05f2511b2b9ed1f7a9b8db3623555b2a0aac5c9e
Mesko <willianmesko@hotmail.com>
2020-01-15 15:39:43 -0300

    Update styles.js

--- Frontend/src/components/TimeInfoBox/styles.js
+++ Frontend/src/components/TimeInfoBox/styles.js
@@ -8,19 +8,33 @@ export const useStyles = makeStyles(theme => ({
     fontFamily: "sans-serif",
     backgroundColor: "#363636",
     opacity: 0.8,
-    color: "white"
+    color: "white",
+    '@media (min-width: 320px) and (max-width: 414px)': {
+      height: "150px"
+    }
   },
   container: {
     paddingBottom: "15px"
   },
   details: {
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    fontSize: "50px",
+    '@media (min-width: 320px) and (max-width: 414px)': {
+      fontSize: "20px"
+    }
   },
 
   cover: {
     width: "140px",
     padding: "15px",
-    margin: "20px"
+    margin: "20px",
+    '@media (min-width: 320px) and (max-width: 414px)': {
+      minWidth: "40px",
+      maxWidth: "40px",
+      minHeight: "80px",
+      maxHeight: "80px"
+
+    }
   }
 }));
