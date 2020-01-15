55892d9fa4f6b6f96a0b36e59f74d69525234220
Mesko <willianmesko@hotmail.com>
2020-01-15 15:35:47 -0300

    Update styles.js

--- Frontend/src/components/Campeonatos/styles.js
+++ Frontend/src/components/Campeonatos/styles.js
@@ -13,8 +13,15 @@ export const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2)
   },
   grid: {
+    width: "120px",
+    height: "120px",
     "&:hover": {
       opacity: 0.7
-    }
+    },
+    '@media (min-width: 320px) and (max-width: 414px)': {
+      width: "80px",
+      height: "80px",
+    },
+   
   }
 }));
