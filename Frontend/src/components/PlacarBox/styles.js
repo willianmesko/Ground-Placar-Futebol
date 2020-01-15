80bfb09db1f7cc90fecdab35f9ee3c4312d0389e
Mesko <willianmesko@hotmail.com>
2020-01-15 15:39:03 -0300

    Update styles.js

--- Frontend/src/components/PlacarBox/styles.js
+++ Frontend/src/components/PlacarBox/styles.js
@@ -12,6 +12,15 @@ export const useStyles = makeStyles({
     "&:hover": {
       backgroundColor: "#3f51b5",
       opacity: 0.7
+    },
+    '@media (max-width: 320px)': {
+      height: "110px",
+    },
+    '@media (max-width: 375px)': {
+      height: "130px",
+    },
+    '@media (max-width: 414px)': {
+      height: '150px'
     }
   },
   title: {
@@ -23,34 +32,41 @@ export const useStyles = makeStyles({
   cardContent: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    '@media (min-width: 320px) and (max-width: 414px) ': {
+      height: "15px"
+     },
   },
   champ: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-
     fontSize: "30px",
     marginTop: "5px",
     fontWeight: "600",
-    marginBottom: "4px"
+    marginBottom: "4px",
+    '@media (min-width: 320px) and (max-width: 414px)': {
+      fontSize: "15px",
+      marginBottom: "10px",
+    },
   },
   fase: {
-    textAlign: "center"
+    textAlign: "center",
+    '@media (min-width: 320px) and (max-width: 414px)': {
+      fontSize: "8px",
+      marginBottom: "15px"
+    },
   },
   placar: {
     fontSize: "60px"
   }
 });
 
-export const PlacarInfo = styled.div`
+export const TimeInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-left: 20px;
-  padding-left: 20px;
-  border-left: 1px solod #eee;
   text-decoration: none;
 
   span {
@@ -59,13 +75,21 @@ export const PlacarInfo = styled.div`
     color: #eee;
     font-weight: 600;
     margin-top: 4px;
+    @media (min-width: 320px) and (max-width: 414px) {
+      font-size: 10px;
+    }
   }
   img {
     height: 100px;
     border-radius: 50%;
+    @media (min-width: 320px)  and (max-width: 414px) {
+      min-width: 40px;
+      min-height: 40px;
+      max-width: 60px;
+      max-height: 60px;
+   }
   }
   a {
-    display: inline-block;
     text-decoration: none;
   }
   a:hover {
@@ -77,3 +101,18 @@ export const PlacarInfo = styled.div`
     font-size: 20px;
   }
 `;
+
+
+export const PlacarInfo = styled.div`
+  p {
+    font-size: 60px;
+    @media (min-width: 320px) and (max-width: 414px) {
+      font-size: 20px;
+      padding-top: 5px;
+      padding-left: 15px;
+      padding-bottom: 10px;
+    }
+    
+  }
+  `;
+
